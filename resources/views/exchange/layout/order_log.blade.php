f183c6380151d30dfbf2d06c2962765c99fa0f76
octacondeveloper <kaakadev@gmail.com>
2020-09-15 01:23:22 +0100

    swap

--- resources/views/exchange/layout/order_log.blade.php
+++ resources/views/exchange/layout/order_log.blade.php
@@ -1,18 +1,18 @@
 <div class="col-lg-9 col-md-8  col-sm-12 col-xs-12 custom-float-md">
     <div class="sell-n-buy">
         <div class="" style="position: relative">
-            <app-market-orders _nghost-xmu-c11="">
+            <app-market-orders _nghost-xmu-c11="" style="height: 500px;">
                 <div _ngcontent-xmu-c11="" class="main-app-container buy-sell buyContainer" style="position: relative">
-                    <div _ngcontent-xmu-c11="" id="buy_orders" class="main-open-orders orders-general-box" style="height: 500px;">
+                    <div _ngcontent-xmu-c11="" id="buy_orders" class="main-open-orders orders-general-box">
 
                     </div>
                 </div>
             </app-market-orders>
         </div>
-        <div class="" style="position: relative">
+        <div class="" style="position: relative"  style="height: 500px;">
             <app-market-orders _nghost-xmu-c11="">
                 <div _ngcontent-xmu-c11="" class="main-app-container buy-sell sellContainer" style="position: relative">
-                    <div _ngcontent-xmu-c11="" id="sell_orders" class=" main-open-orders orders-general-box" style="height: 500px;">
+                    <div _ngcontent-xmu-c11="" id="sell_orders" class=" main-open-orders orders-general-box">
 
                     </div>
                 </div>
